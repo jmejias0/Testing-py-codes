d5ba0ee72232da8b45ec8c0be42128a08cf9476f
jmejias0 <joselmz16@gmail.com>
2026-08-26 09:51:03 -0700

    declaring floating point and string variables

--- Python/helloWorld.py
+++ Python/helloWorld.py
@@ -1,4 +1,12 @@
 age = 20
 print(age)
 
-print("Hello World")
+print("Hello World")  
+
+# This is a comment in Python
+# The program runs from top to bottom, executing each line of code in order.
+
+price = 18.25
+first_name = "Jose"
+print(price)
+print(first_name)
