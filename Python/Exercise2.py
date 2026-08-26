2f603b35f1cfc46e7a5689da1f91eeb399091e7d
jmejias0 <joselmz16@gmail.com>
2026-08-26 11:32:01 -0700

    Making a very simple calculator to practice

---
+++ Python/Exercise2.py
@@ -0,0 +1,4 @@
+first_number = input("First: ")  # Receiving input from the user and storing it in the variable 'first_number'
+second_number = input("Second: ")  # Receiving input from the user and storing it in the variable 'second_number'
+
+print(float(first_number) + float(second_number))  # Print the sum of the two numbers to the console
