2f66eef296e06aa1348df6ff7502f6e2e6c8ddad
jmejias0 <joselmz16@gmail.com>
2026-08-26 09:54:36 -0700

    Adding comments to each line of code

--- Python/helloWorld.py
+++ Python/helloWorld.py
@@ -1,12 +1,12 @@
-age = 20
-print(age)
+age = 20                # Declaring an integer variable
+print(age)              # Print the value of the variable 'age' to the console
 
 print("Hello World")  
 
 # This is a comment in Python
 # The program runs from top to bottom, executing each line of code in order.
 
-price = 18.25
-first_name = "Jose"
-print(price)
-print(first_name)
+price = 18.25           # Declaring a floating point variable
+first_name = "Jose"     # Declaring a string variable
+print(price)            # Print the value of the variable 'price' to the console
+print(first_name)       # Print the value of the variable 'first_name' to the console
